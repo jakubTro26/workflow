e6cbadf23169a930fd75ce19c7ffd58a930137cf
kuba <info@weblead.pl>
2021-09-21 17:30:04 +0200

    first

--- pull.php
+++ pull.php
@@ -9,5 +9,5 @@ putenv('PATH=/usr/local/bin');
 echo shell_exec("cd /home/workflowtrends/web/workflowtrends.pl/public_html && /usr/bin/git pull origin main 2>&1");
 echo shell_exec('/usr/bin/whoami 2>&1');
 }
-
+//kuba
 ?>
