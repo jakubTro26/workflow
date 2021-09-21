59abc0b3f78ac42d7833af4a113b490324d676a7
kuba <info@weblead.pl>
2021-09-21 17:24:12 +0200

    first

---
+++ pull.php
@@ -0,0 +1,13 @@
+<?php
+
+// Use in the “Post-Receive URLs” section of your GitHub repo.
+
+
+
+if ( $_POST['payload'] ) {
+putenv('PATH=/usr/local/bin');
+echo shell_exec("cd /home/workflowtrends/web/workflowtrends.pl/public_html && /usr/bin/git pull origin main 2>&1");
+echo shell_exec('/usr/bin/whoami 2>&1');
+}
+
+?>
